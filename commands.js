8c21e3c15223ae923f2fa41044bf7e345d7196b7
RoyalKrK <108145789+RoyalKrK@users.noreply.github.com>
2022-08-01 17:23:32 +0530

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,6 @@
+function myfunction(){
+    window.location.href="https://www.facebook.com/krithik.chandrasekar";
+  }
+function myfunction2(){
+    window.location.href="https://t.snapchat.com/KV9UkDvj";
+  }
