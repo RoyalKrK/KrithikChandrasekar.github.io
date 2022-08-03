22af41c7f8985cb6b14750283da7d7d8c591e3fa
RoyalKrK <108145789+RoyalKrK@users.noreply.github.com>
2022-08-03 19:36:06 +0530

    Button fix

--- commands.js
+++ commands.js
@@ -4,3 +4,6 @@ function myfunction(){
 function myfunction2(){
     window.location.href="https://t.snapchat.com/KV9UkDvj";
   }
+function myfunction3(){
+    window.location.href="https://www.instagram.com/royal__krk/";
+  }
